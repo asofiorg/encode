53863fbe487c8bd252556486876e787e111a6533
Juan Almanza <hi@scidroid.co>
2022-08-20 22:09:57 -0500

    add header clickable

--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,18 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className="flex items-center my-12">
-      <Image src="/logo.svg" alt="logo" height={61} width={61} />
-      <p className="flex ml-4 text-3xl">
-        <span className="font-bold md:hidden mr-2">EJ</span>
-        <span className="font-bold hidden md:block mr-2">Encode Justice</span>
-        Colombia
-      </p>
-    </header>
+    <Link href="/">
+      <header className="flex items-center my-12 cursor-pointer">
+        <Image src="/logo.svg" alt="logo" height={61} width={61} />
+        <p className="flex ml-4 text-3xl">
+          <span className="font-bold md:hidden mr-2">EJ</span>
+          <span className="font-bold hidden md:block mr-2">Encode Justice</span>
+          Colombia
+        </p>
+      </header>
+    </Link>
   );
 };
 
